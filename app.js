e18ae29542198bac01f4a6a01496d3d49818bbdc
Mark Mwima <markmwimar@gmail.com>
2023-09-20 09:52:46 +0300

    if statement testing

--- app.js
+++ app.js
@@ -39,11 +39,20 @@ function countChars(event) {
 
     // updating Remaining characters
     remainingChars.textContent = charsRemaining;
+
+    if (charsRemaining <=15) {
+        remainingChars.classList.add('warning');
+        inputData.classList.add('warning');
+        
+    } else {
+        remainingChars.classList.remove('warning');
+        inputData.classList.remove('warning');
+        
+    }
 }
 
 inputData.addEventListener('input', countChars);
 
-remainingChars.style.color = 'teal';
 remainingChars.style.fontSize = '1rem';
 
 
